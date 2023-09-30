22f13a12c4a342f56b8fe9fd6045e266c5a7f28f
mazaya.nur11 <mazaya.nur11@ui.ac.id>
2023-09-30 10:50:08 +0700

    tugas individu - initialize spring boot project

---
+++ silogistik2106639485/src/main/java/apap/ti/silogistik2106639485/Silogistik2106639485Application.java
@@ -0,0 +1,13 @@
+package apap.ti.silogistik2106639485;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Silogistik2106639485Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Silogistik2106639485Application.class, args);
+	}
+
+}
